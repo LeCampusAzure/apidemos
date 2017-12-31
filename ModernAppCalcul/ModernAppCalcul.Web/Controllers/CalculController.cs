35428cbf7b21c41522418413ae608ec187f0dddd
Marius Zaharia <marius@lecampusazure.net>
2017-12-31 14:52:03 +0000

    Update - front app

--- ModernAppCalcul/ModernAppCalcul.Web/Controllers/CalculController.cs
+++ ModernAppCalcul/ModernAppCalcul.Web/Controllers/CalculController.cs
@@ -15,23 +15,29 @@ namespace ModernAppCalcul.Web.Controllers
 {
     public class CalculController : Controller
     {
+
+        const string UriServiceCalcul = "https://[your-api].azure-api.net/CalculMensualite";
+        const string CleServiceApim = "[your-key]";
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public async Task<IActionResult> Resultat(double capital, double tauxAnnuelPrc, int duree)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Resultat(CalculMensualite calculMens)
         {
             string resultat = "";
 
             try
             {
-                var response = await AppelResultatAsync(capital, tauxAnnuelPrc, duree);
+                var response = await AppelResultatAsync(calculMens);
                 if (response.IsSuccessStatusCode)
                 {
                     string resultatTemp = await response.Content.ReadAsStringAsync();
                     CalculMensualiteResponse calculResponse = JsonConvert.DeserializeObject<CalculMensualiteResponse>(resultatTemp);
-                    resultat = calculResponse.Resultat.ToString("C");
+                    resultat = calculResponse.Resultat.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("fr-fr"));
                 }
                 else
                 {
@@ -49,27 +55,25 @@ namespace ModernAppCalcul.Web.Controllers
             return View();
         }
 
-        private async Task<HttpResponseMessage> AppelResultatAsync(double capital, double tauxAnnuelPrc, int duree)
+       
+        private async Task<HttpResponseMessage> AppelResultatAsync(CalculMensualite calculMens)
         {
             HttpClient client = new HttpClient();
 
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "[your-sub-key]");
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", CleServiceApim);
 
-            dynamic calcul = new {
-                    calculMensualite = new CalculMensualite()
-                    {
-                        Capital = capital,
-                        TauxAnnuelPrc = tauxAnnuelPrc,
-                        DureeEnMois = duree
-                    }
-                };
+    
+            dynamic calcul = new
+            {
+                calculMensualite = calculMens
+            };
 
             string calculSerialized = JsonConvert.SerializeObject(calcul) ;
             var stringContent = new StringContent(calculSerialized, Encoding.UTF8, "application/json");
-            return await client.PostAsync("https://[your-apim].azure-api.net/CalculMensualite", stringContent);
+            return await client.PostAsync(UriServiceCalcul, stringContent);
             
         }
     }
